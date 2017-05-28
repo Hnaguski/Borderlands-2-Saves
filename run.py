2cdbb9d401b0148dbc450485c2beb19546eb3575
Hnaguski <hnaguski@gmail.com>
2017-05-27 21:11:34 -0400

    'Save Updated'

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from subprocess import call
 call(["git", "add", "."])
-call(["git", "commit", "-a", "-m", "'\Save Updated\'"])
+call(["git", "commit", "-a", "-m", "\'Save Updated\'"])
 call(["git", "push", "origin", "master"])
